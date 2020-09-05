45e5d8bf13c5d1e2b55cada1416c6a6093564a6d
GrinchFour <58406613+GrinchFour@users.noreply.github.com>
2020-09-05 13:33:08 +1000

    Add files via upload

---
+++ mycsharp_lesson2/mycharp_lesson2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks.Dataflow;
+
+namespace mycharp_lesson2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            a = 35000;
+            int b, c;
+            bool d;
+            d = true;
+            long e = 10;
+            float f = 5.5f;
+            char g = 'g';
+            short num = 10;
+            num = (short) a;
+            
+        }
+    }
+}
---
+++ mycsharp_lesson6/mycsharp_lesson6/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace mycsharp_lesson6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] massive_1 = new int[10];
+            for (int i = 0; i < massive_1.Length; i++)
+                massive_1[i] = i + 1; 
+            int[] massive_2 = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            int[] massive_res = new int[10];
+            for (int i = 0; i < massive_res.Length; i++)
+                Console.WriteLine("Элемент под индексом " + i + " --> " + (massive_res[i] = massive_1[i] + massive_2[i]));
+        }
+    }
+}
+
+/// Задача 3. Дано два массива одинаковой длины (по 10 элементов). 
+/// Создайте третий массив, который будет отображать сумму первых двух массивов. 
+/// Первый элемент третьего массива равен сумме первых элементов двух первых массивов и так далее.
