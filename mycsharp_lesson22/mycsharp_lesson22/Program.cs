ee4c66c4a72b85e293917bdbad054f1d9400e6d7
GrinchFour <58406613+GrinchFour@users.noreply.github.com>
2020-10-07 20:42:28 +1000

    Add files via upload

---
+++ mycsharp_lesson22/mycsharp_lesson22/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace mycsharp_lesson22
+{
+    interface ISwitchable
+    {
+        void SwitchMe(bool sw);
+    }
+
+    interface ILight
+    {
+        void LightMe(string lt);
+    }
+
+    class veryImportantDetail : ISwitchable, ILight
+    {
+        private bool sw;
+        private string lt;
+
+        public void SwitchMe(bool sw)
+        {
+            this.sw = sw;
+        }
+
+        public void LightMe(string lt)
+        {
+            this.lt = lt;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
